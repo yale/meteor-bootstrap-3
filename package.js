a20e3a468efab3e8f8fa4bc08b504ed94030ffb9
Ahmet Yasin Uslu <nepjua@gmail.com>
2013-05-01 19:36:13 +0300

    configured paths

--- package.js
+++ package.js
@@ -7,9 +7,15 @@ Package.on_use(function (api) {
 
   var path = Npm.require('path');
   var asset_path = path.join('bootstrap-3', 'docs', 'assets');
-  api.add_files(asset_path.join('css', 'bootstrap.css'), 'client');
-  api.add_files(asset_path.join('js', 'bootstrap.js'), 'client');
-  api.add_files(asset_path.join('fonts', 'glyphicons-halflings.png'), 'public');
+  api.add_files(path.join(asset_path, 'css', 'bootstrap.css'), 'client');
+  api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
+
+  // fonts
+  api.add_files(path.join(asset_path, 'fonts', 'glyphiconshalflings-regular.eot'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphiconshalflings-regular.otf'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphiconshalflings-regular.ttf'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphiconshalflings-regular.svg'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'glyphiconshalflings-regular.woff'), 'client');
 
   // XXX this makes the paths to the icon sets absolute. it needs
   // to be included _after_ the standard bootstrap css so
