201203f1469be2485786cdf29428c8b608129c89
Ahmet Yasin Uslu <nepjua@gmail.com>
2013-05-02 03:41:37 +0300

    updated paths

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.on_use(function (api) {
   api.use('jquery');
 
   var path = Npm.require('path');
-  var asset_path = path.join('bootstrap-3', 'docs', 'assets');
+  var asset_path = path.join('bootstrap-3');
   api.add_files(path.join(asset_path, 'css', 'bootstrap.css'), 'client');
   api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
 
