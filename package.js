8026f0beaa0274849adfcf8c53d919216b5e06ba
Osman Alperen Elhan <alperen@elhan.org>
2013-07-09 03:33:22 +0300

    forgot to include file in package.js

--- package.js
+++ package.js
@@ -8,6 +8,7 @@ Package.on_use(function (api) {
   var path = Npm.require('path');
   var asset_path = path.join('bootstrap-3');
   api.add_files(path.join(asset_path, 'css', 'bootstrap.css'), 'client');
+  api.add_files(path.join(asset_path, 'css', 'bootstrap-glyphicons.css'), 'client');
   api.add_files(path.join(asset_path, 'js', 'bootstrap.js'), 'client');
 
   // fonts
