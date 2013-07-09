985f30bb70050bdf1f956ab082a9c6d720e65cb6
Osman Alperen Elhan <alperen@elhan.org>
2013-07-09 03:28:53 +0300

    merge gylphicons with bootstrap

--- update.sh
+++ update.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-cd "git-repo"
+rm -rf bootstrap-3/fonts
+rm -rf bootstrap-3/css/bootstrap-glyphicons.css
+cd "git-repo/bootstrap"
 rm -rf bootstrap/
 git checkout -- .
 git pull
 make bootstrap
-cp bootstrap/* ../bootstrap-3/ -r
+cp bootstrap/* ../../bootstrap-3/ -r
+cd "../bootstrap-glyphicons"
+git checkout -- .
+git pull
+cp -r "fonts" ../../bootstrap-3
+cp "css/bootstrap-glyphicons.css" ../../bootstrap-3/css
